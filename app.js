17eb3d6799c3ebf148e83b63089461ae3e7a1efb
JeremieBrun88 <entreprise.brunj@gmail.com>
2025-05-26 11:03:40 +0200

    Lundi 11h03

--- app.js
+++ app.js
@@ -1,40 +1,55 @@
 fetch("data.json")
-    .then(response => response.json())
-    .then(data => {
-
-
-        console.log(data)
-        afficher(data)
+    .then(reponse => reponse.json())
+    .then(donnee => {
+        afficherProduits(donnee.produits)
+        afficherClients(donnee.clients)
     })
 
 
-// je récupère les éléments du Html HERO //
+//role : afficher les produits dans #produits
+//paramùetre: le tableau produits
+//return : rien
 
-
-document.getElementById("hero").innerHTML +=
-
-    `
-     
-  `
-
-
-
-// je mets une fonction pour afficher les produits //
-
-
-function afficher(produits) {
-
-
-    produits.forEach(produits => {
-
-          console.log(produits)
-
-        afficher(produits)
+function afficherProduits(tableauProduits) {
+    tableauProduits.forEach(produit => {
 
         document.getElementById("produits").innerHTML += `
-        
-         
-          
-        `
-    })
+
+    
+         <div class=" card">
+                <img class="rad" src="${produit.imageurl}" alt="Image produits">
+                <h3 class="font25">${produit.titre}</h3>
+                <p class="font15">${produit.presentation}</p>
+            </div>
+    
+    `
+    });
 }
+
+//role : afficher les avis clients
+// parametre : le tableau des avis clients
+//return : rien
+
+function afficherClients(tableauClients) {
+    tableauClients.forEach(clients => {
+
+        document.getElementById("clients").innerHTML += `
+
+        <div class="flex textCenter cardAvis">
+             <ul class="">
+               <li class="font15">Sophie</li>
+              <li class="font15">Coffret cadeau personnalisé</li>
+               <li>Le coffret de chocolats que j'ai offert était magnifique et délicieux. Les saveurs étaient
+                  variées et originales, et la présentation était très soignée. Un grand merci au Palais du
+                 Chocolat pour ce cadeau parfait.</li>
+              <li class="font15">Note 5/5</li>
+             </ul>
+        </div>
+    
+    `
+    });
+}
+
+
+
+
